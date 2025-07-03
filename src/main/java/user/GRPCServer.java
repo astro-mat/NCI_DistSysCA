868068a8f6287ba683d95dd8b7813f4ac96d344f
astro-mat <whoismatwarren@gmail.com>
2025-07-03 14:47:36 +0100

    Create GRPCServer.java

---
+++ src/main/java/user/GRPCServer.java
@@ -0,0 +1,25 @@
+package user;
+
+import java.io.IOException;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class GRPCServer {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		
+		
+		Server server = ServerBuilder.forPort(9090).addService(new SmartAttendanceMonitorService()).build();
+		
+		server.start();
+		
+		System.out.println("Server started at " + server.getPort());
+		
+		server.awaitTermination();
+		
+		
+	}
+
+}

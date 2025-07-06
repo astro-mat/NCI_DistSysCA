3d762c905cb741c19fb4ae49796c2b12c66948a6
astro-mat <whoismatwarren@gmail.com>
2025-07-06 18:50:00 +0100

    begin remaining client files

---
+++ src/main/java/com/eLearningSystem/client/StudentLearningAssistantClient.java
@@ -0,0 +1,23 @@
+package com.eLearningSystem.client;
+
+
+import com.eLearningSystem.grpc.StudentLearningAssistantGrpc;
+import com.eLearningSystem.grpc.StudentLearningAssistantGrpc.StudentLearningAssistantBlockingStub;
+import com.eLearningSystem.grpc.StudentLearningAssistantGrpc.StudentLearningAssistantStub;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class StudentLearningAssistantClient {
+	// main method
+	public static void main(String[] args) {
+		
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090).usePlaintext().build();
+		
+		// stubs - generate from proto file
+		StudentLearningAssistantBlockingStub studentLearningAssistantStub = StudentLearningAssistantGrpc.newBlockingStub(channel)
+
+		
+	}
+
+}
